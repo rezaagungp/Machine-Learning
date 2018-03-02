dd48846c48548a0c1431cb7204706c5ba163a1ee
rezaagungp <rezaagungpambudi@gmail.com>
2018-03-02 15:02:20 +0700

    train data for Rosenblatt and adaline model using iris dataset

--- Artificial Neurons/Train.py
+++ Artificial Neurons/Train.py
@@ -1,6 +1,7 @@
-import Model
 import numpy as np
+import AdalineModel
 import pandas as pd
+import RosenblattModel
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
@@ -49,7 +50,7 @@ plt.legend(loc= 'upper left')
 plt.show()
 
 """training data and show the result in label"""
-nn = Model.Perceptron(eta=0.1, n_iter = 10)
+nn = RosenblattModel.Perceptron(eta=0.1, n_iter = 10)
 nn.fit(X,y)
 plt.plot(range(1,len(nn.errors_)+1),nn.errors_, marker='o')
 plt.xlabel('Epochs')
@@ -63,24 +64,39 @@ plt.ylabel('petal length [cm]')
 plt.legend(loc='upper left')
 plt.show()
 
-
+"""plot SSE score for learing rate 0.01"""
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8,4))
-ada1 = Model.AdalineGD(n_iter=10, eta=0.01).fit(X,y)
+ada1 = AdalineModel.AdalineGD(n_iter=10, eta=0.01).fit(X,y)
 ax[0].plot(range(1, len(ada1.cost_)+1),np.log10(ada1.cost_), marker='o')
 ax[0].set_xlabel('Epochs')
 ax[0].set_ylabel('log(Sum-squared-error)')
 ax[0].set_title('Adaline - Learning rate 0.01')
 
-ada2 = Model.AdalineGD(n_iter=10, eta=0.0001).fit(X,y)
+"""plot SSE score for learing rate 0.0001"""
+ada2 = AdalineModel.AdalineGD(n_iter=10, eta=0.0001).fit(X,y)
 ax[1].plot(range(1, len(ada2.cost_)+1), ada2.cost_, marker='o')
 ax[1].set_xlabel('Epochs')
 ax[1].set_ylabel('Sum-Squared-error')
 ax[1].set_title('Adaline - Learning rate 0.0001')
 plt.show()
 
-#data normalization
+"""data normalization"""
 X_nor = np.copy(X)
 X_nor[:,0] = (X[:,0] - X[:,0].mean())/X[:,0].std()
-X_nor[:,1] = (X[:,1] - X[:,1].mean())/X[:,0].std()
-ada = Model.AdalineGD(n_iter=15, eta=0.1)
+X_nor[:,1] = (X[:,1] - X[:,1].mean())/X[:,1].std()
+
+"""plot decision regions after traing data using adaline"""
+ada = AdalineModel.AdalineGD(n_iter=15, eta=0.01)
 ada.fit(X_nor,y)
+decision_regions(X_nor,y, classifier=ada)
+plt.title('Adaline - Gradient Descent')
+plt.xlabel('sepal length [normalization]')
+plt.ylabel('petal length [normalization]')
+plt.legend(loc='upper left')
+plt.show()
+
+"""plot SSE for normalization"""
+plt.plot(range(1, len(ada.cost_)+1), ada.cost_, marker = 'o')
+plt.xlabel('Epochs')
+plt.ylabel('Sum-squared-error')
+plt.show()
