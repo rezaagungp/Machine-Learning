922969968acd6b0fedcf26a22a1e7780c79263d1
rezaagungp <rezaagungpambudi@gmail.com>
2018-03-02 22:27:17 +0700

    update training plot

--- Artificial Neurons/Train.py
+++ Artificial Neurons/Train.py
@@ -1,36 +1,11 @@
+import sys
 import numpy as np
 import AdalineModel
 import pandas as pd
 import RosenblattModel
+sys.path.append('../')
 import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
-
-
-def decision_regions(X, y, classifier, resolution=0.02):
-        
-    #setup generator and color map
-    markers = ('s', 'x', 'o', '^', 'v')
-    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
-    cmap = ListedColormap(colors[:len(np.unique(y))])
-
-    #plot decision surface
-    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-                           np.arange(x2_min, x2_max, resolution))
-    
-    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-    Z = Z.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
-    plt.xlim(xx1.min(), xx1.max())
-    plt.ylim(xx2.min(), xx2.max())
-
-    #plot data
-    for idx, cl in enumerate(np.unique(y)):
-        plt.scatter(x=X[y == cl, 0], y=X[y == cl,1],
-                    alpha=0.8, c=cmap(idx),
-                    marker=markers[idx], label=cl)
-
+from DecisionRegions import plot_regions
 
 """fetch the iris dataset"""
 df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
@@ -58,7 +33,7 @@ plt.ylabel('Number of misclassifications')
 plt.show()
 
 """plot training result using decision boundary"""
-decision_regions(X,y, classifier=nn)
+plot_regions(X,y, classifier=nn)
 plt.xlabel('sepal length [cm]')
 plt.ylabel('petal length [cm]')
 plt.legend(loc='upper left')
@@ -88,7 +63,7 @@ X_nor[:,1] = (X[:,1] - X[:,1].mean())/X[:,1].std()
 """plot decision regions after traing data using adaline"""
 ada = AdalineModel.AdalineGD(n_iter=15, eta=0.01)
 ada.fit(X_nor,y)
-decision_regions(X_nor,y, classifier=ada)
+plot_regions(X_nor,y, classifier=ada)
 plt.title('Adaline - Gradient Descent')
 plt.xlabel('sepal length [normalization]')
 plt.ylabel('petal length [normalization]')
