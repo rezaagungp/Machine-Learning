a0d00fe5fa4253d0fe6e21dc1911cc23a925cf71
rezaagungp <rezaagungpambudi@gmail.com>
2018-03-02 14:01:54 +0700

    organized folder

--- Artificial Neurons/Train.py
+++ Artificial Neurons/Train.py
@@ -62,3 +62,25 @@ plt.xlabel('sepal length [cm]')
 plt.ylabel('petal length [cm]')
 plt.legend(loc='upper left')
 plt.show()
+
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8,4))
+ada1 = Model.AdalineGD(n_iter=10, eta=0.01).fit(X,y)
+ax[0].plot(range(1, len(ada1.cost_)+1),np.log10(ada1.cost_), marker='o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Sum-squared-error)')
+ax[0].set_title('Adaline - Learning rate 0.01')
+
+ada2 = Model.AdalineGD(n_iter=10, eta=0.0001).fit(X,y)
+ax[1].plot(range(1, len(ada2.cost_)+1), ada2.cost_, marker='o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('Sum-Squared-error')
+ax[1].set_title('Adaline - Learning rate 0.0001')
+plt.show()
+
+#data normalization
+X_nor = np.copy(X)
+X_nor[:,0] = (X[:,0] - X[:,0].mean())/X[:,0].std()
+X_nor[:,1] = (X[:,1] - X[:,1].mean())/X[:,0].std()
+ada = Model.AdalineGD(n_iter=15, eta=0.1)
+ada.fit(X_nor,y)
